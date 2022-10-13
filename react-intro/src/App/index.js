1d44c2ac1a3688e190b979bf426c8cfd1b0e1884
Elvis <elviscruz45@gmail.com>
2022-10-12 20:48:14 -0500

    finished

--- react-intro/src/App/index.js
+++ react-intro/src/App/index.js
@@ -11,21 +11,61 @@ const defaultTodos=[
   {text:"LALALALALA",completed:false},
 ]
 
-function App() {
-  const localStorageTodos=localStorage.getItem("TODOS_V1")
-  let parsedTodos;
-  if(!localStorageTodos){
-    localStorage.setItem("TODOS_V1",JSON.stringify([]))
-    parsedTodos=[]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function useLocalStorage(itemName,initialValue){
+
+  const localStorageItem=localStorage.getItem(itemName)
+  let parsedItem;
+  if(!localStorageItem){
+    localStorage.setItem(itemName,JSON.stringify(initialValue))
+    parsedItem=initialValue
 
   }else{
-    parsedTodos=JSON.parse(localStorageTodos)
+    parsedItem=JSON.parse(localStorageItem)
+  }
+  const [item,setItem]=React.useState(parsedItem)
+
+  const saveItem=(newItem)=>{
+    const stringifiedItem=JSON.stringify(newItem)
+    localStorage.setItem(itemName,stringifiedItem)
+    setItem(newItem)
   }
 
+  return [
+    item,
+    saveItem,
+  ]
 
+}
+
+
+
+
+function App() {
+  const [todos,saveTodos]=useLocalStorage("TODOS_V1",[])
 
   //Todo counter
-  const [todos,setTodos]=React.useState(parsedTodos)
   const totalTodos=todos.length
   const completedTodos=todos.filter(todo=>!!todo.completed).length
 
@@ -42,12 +82,7 @@ function App() {
       return todoText.includes(searchText)
     })
   }
-  const saveTodos=(newTodos)=>{
-    const stringifiedTodos=JSON.stringify(newTodos)
-    localStorage.setItem("TODOS_V1",stringifiedTodos)
-    setTodos(newTodos)
 
-  }
 
   const completeTodos=(text)=>{
     const todoIndex=todos.findIndex(todo=>todo.text===text)
